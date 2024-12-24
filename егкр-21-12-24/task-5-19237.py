cd9f428b281fd953c0ddfe5c356988a96e39c9f2
DTolstyh <liubovtolstih@gmail.com>
2024-12-24 18:55:35 +0200

    тестовый коммит

--- егкр-21-12-24/task-5-19237.py
+++ егкр-21-12-24/task-5-19237.py
@@ -1,3 +1,24 @@
-print('hello')
+#номер 5
+#создать функцию для перевода в другую сис счисления
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+ans = []
 
-print('world')
+for N in range(1, 10000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R = R + R[:len(R) - 2]
+    else:
+        R = R + convert(sum(map(int, R)), 3)
+    R = int(R)
+    if R % 2 == 0 and R > 220:
+        ans.append(R)
+print(min(ans))
+
+
+#номер 14
+
