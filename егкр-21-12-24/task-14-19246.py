d5260de0e14b273b9051e54d7036853450fa5dff
DTolstyh <liubovtolstih@gmail.com>
2024-12-24 19:11:51 +0200

    адание 14-19246 импортирование из строки и работа с переводом слагаемых из 25ричной системы

---
+++ егкр-21-12-24/task-14-19246.py
@@ -0,0 +1,9 @@
+from string import digits, ascii_uppercase
+
+alph = digits + ascii_uppercase
+ans = []
+for x in alph[:25]:
+    num = int(f'11353{x}12', 25) + int(f'135{x}21', 25)
+    if num % 24 == 0:
+        ans.append([x, num // 24])
+print(max(ans))
