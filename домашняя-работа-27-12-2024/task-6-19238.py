db9af49b8b6a4d089c020c404a212a2adfd9e51d
DanilT-20 <liubovtolstih@gmail.com>
2024-12-27 11:36:54 +0300

    номер 6 - черепаха

---
+++ домашняя-работа-27-12-2024/task-6-19238.py
@@ -0,0 +1,20 @@
+from turtle import *
+tracer(0)
+m = 6
+
+for x in range(8):
+    fd(16*m)
+    rt(90)
+    fd(22*m)
+    rt(90)
+up()
+fd(5*m)
+rt(90)
+fd(5*m)
+lt(90)
+down()
+for x in range(8):
+    fd(52*m)
+    rt(90)
+    fd(77*m)
+    rt(90)
