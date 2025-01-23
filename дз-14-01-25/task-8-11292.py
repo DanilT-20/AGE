a38bf033818637b3f53b021d0f13cbd779852923
DanilT-20 <liubovtolstih@gmail.com>
2025-01-23 21:14:12 +0300

    решение 8 задания с помощью кода и генератора

--- дз-14-01-25/task-8-11292.py
+++ дз-14-01-25/task-8-11292.py
@@ -1,5 +1,8 @@
 from itertools import product
+count = 0
 for i in product('0123456789ABCDEF', repeat=5):
     i = ''.join(i)
-    i = [1 for p in ''.join(str(product('02468ACE', repeat=2))) if (i[0] != '0' and i.count('6') == 2 and p not in i)]
-print(len(i))
+    for p in '02468ACE':
+        if i[0] != '0' and i.count('6') == 2 and p + '6' not in i and '6' + p not in i:
+            count += 1
+print(count)
