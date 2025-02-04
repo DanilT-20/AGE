0173151ebb8f32f57d9c0e160d56314b6007e87a
DTolstyh <liubovtolstih@gmail.com>
2025-02-04 19:45:29 +0200

    решение 15 задания с отрезками с помощью кода

---
+++ егкр-04-02-25/task1.py
@@ -0,0 +1,10 @@
+from string import digits, ascii_uppercase
+from itertools import product
+alph = digits + ascii_uppercase
+count = 0
+for v in product(alph[:20], repeat=5):
+    v = ''.join(v)
+    if v[0] in alph[::2] and v[1] in alph[1::2] and v[2] in alph[::2] and v[3] in alph[1::2] and v[4] in alph[::2] or v[0] in alph[1::2] and v[1] in alph[::2] and v[2] in alph[1::2] and v[3] in alph[::2] and v[4] in alph[1::2]:
+        if int(v[0], 20) + int(v[-1], 20) == 26 and v[0] != 0:
+            count += 1
+print(count)
---
+++ егкр-04-02-25/task2.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+count = 0
+for v in set(permutations('КИДАЛА', 5)):
+    v = ''.join(v)
+    if 'АА' not in v:
+        count += 1
+print(count)
